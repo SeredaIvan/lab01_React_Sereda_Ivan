0f8b80e79093f5db838865ca3cd578cbe4f9965c
SeredaIvan <vanosereda77@gmail.com>
2024-11-13 16:12:13 +0200

    Created Cells array

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,28 @@
 import Cell from "./Cells.jsx";
 import x from "../../public/images/x.svg";
 import o from "../../public/images/o.svg";
+import {useState} from "react";
+import {isRequiredPropType} from "eslint-plugin-react/lib/util/props.js";
 
 function Board() {
-    const array = Array.from({ length: 9 }, () => ({ type: null }));
+    //turn? obj?x:obj?o
+    const [turn,setTurn]=useState(false)
+
+    let [cells,setCells] = useState(
+        Array.from({ length: 9 }, () => ({ type: null ,clicked:false}))
+    )
+    const handleClick=(index)=>{
+        if (cells[index].clicked) return;
+        const newCells=cells.map((cell,i)=>
+            i === index ? { type: o, clicked: true } : cell
+        )
+        setCells(newCells);
+    }
 
     return (
         <div className="flex-container">
-            {array.map((obj, index) => (
-                <Cell key={index} type={obj.type} />
+            {cells.map((obj, index) => (
+                <Cell key={index} type={obj.type} click={handleClick(index)}/>
             ))}
             <p>Ваш хід</p>
         </div>
--- src/components/Cells.jsx
+++ src/components/Cells.jsx
@@ -1,7 +1,7 @@
 function Cell(props) {
     // отримуємо з пропсу інфу про що ми маємо тут поставити
     return (
-        <div className="block">
+        <div className="block" onClick={props.click}>
             {props.type !== null && (
                 <img className="imgButtons" src={props.type}  />
             )}
