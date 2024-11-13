18a8c12d18841978671ba49691d5ef7ce1744611
SeredaIvan <vanosereda77@gmail.com>
2024-11-13 16:36:15 +0200

    Created Turns iterator

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,31 +1,42 @@
 import Cell from "./Cells.jsx";
 import x from "../../public/images/x.svg";
 import o from "../../public/images/o.svg";
-import {useState} from "react";
-import {isRequiredPropType} from "eslint-plugin-react/lib/util/props.js";
+import { useState } from "react";
 
 function Board() {
-    //turn? obj?x:obj?o
-    const [turn,setTurn]=useState(false)
+    const [iteratorTurn, setIteratorTurn] = useState(0);
+    const [turn, setTurn] = useState(true);
 
-    let [cells,setCells] = useState(
-        Array.from({ length: 9 }, () => ({ type: null ,clicked:false}))
-    )
-    const handleClick=(index)=>{
+    const [cells, setCells] = useState(
+        Array.from({ length: 9 }, () => ({ type: null, clicked: false }))
+    );
+
+    const handleClick = (index) => {
         if (cells[index].clicked) return;
-        const newCells=cells.map((cell,i)=>
-            i === index ? { type: o, clicked: true } : cell
-        )
+        if (iteratorTurn===0){
+            setIteratorTurn(iteratorTurn + 1);
+            return;
+        }
+        const newCells = cells.map((cell, i) =>
+            i === index ? { type: turn ? x : o, clicked: true } : cell
+        );
         setCells(newCells);
-    }
+        setTurn(!turn);
+        setIteratorTurn(iteratorTurn + 1);
+        console.log(iteratorTurn);
+    };
 
     return (
         <div className="flex-container">
             {cells.map((obj, index) => (
-                <Cell key={index} type={obj.type} click={handleClick(index)}/>
+                <Cell key={index} type={obj.type} onClick={() => handleClick(index)} />
             ))}
-            <p>Ваш хід</p>
+            <div className="iterator">
+                <p style={{color:"black"}}>Ваш хід</p>
+                <img style={{marginLeft:10}} height={40} width={40} src={turn ? x : o} />
+            </div>
         </div>
+
     );
 }
 
